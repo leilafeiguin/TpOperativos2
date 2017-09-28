acaf61c4e058eac5ffe1326be1afe8b789ea8b37
MatiasErratchu <erratchumatias@gmail.com>
2017-09-27 21:47:02 -0300

    Implementada tabla de estados

--- Yama/src/Yama.h
+++ Yama/src/Yama.h
@@ -1,5 +1,6 @@
 #include <socketConfig.h>
 #include <commons/log.h>
+#include <commons/collections/list.h>
 
 //ESTRUCTURA ARCHIVO CONFIGURACION
 typedef struct yama_configuracion {
@@ -9,6 +10,33 @@ typedef struct yama_configuracion {
 	char* ALGORITMO_BALANCEO;
 } yama_configuracion;
 
+typedef enum t_etapa {
+	transformacion = 1,
+	reduccionLocal = 2,
+	reduccionGlobal = 3,
+	almacenamientoFinal = 4
+} t_etapa;
+
+typedef enum t_estado {
+	enProceso = 1,
+	error = 2,
+	finalizado = 3
+} t_estado;
+
+typedef struct t_job{
+	int master;
+	int nodo;
+	int bloque;
+	t_etapa etapa;
+	int cantidadTemporal;
+	char* temporal;
+	t_estado estado;
+} t_job;
+
+typedef struct t_estados{
+	int job;
+	t_list* contenido;
+} t_estados;
 
 const char* path = "/home/utnso/Desktop/tp-2017-2c-Todo-ATR/Yama/configYama.cfg";
 
@@ -24,5 +52,14 @@ yama_configuracion get_configuracion() {
 	return configuracion;
 }
 
+void procesar_job(int job, t_job datos);
 
 void sig_handler(int);
+
+void* buscar_por_jobid(int jobId);
+
+void* buscar_por_nodo (int nodo, t_list* listaNodos);
+
+void setearJob(t_job* nuevoJob, t_job datos);
+
+t_job* crearJob(t_job datos);
