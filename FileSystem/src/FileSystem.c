4456e29556159e0d40e9d0a7fd8fc6f100a66961
leilafeiguin <leilafeiguin@gmail.com>
2017-09-28 09:57:27 -0300

    Parseo y validacion de todos los comandos de la consola FS

--- FileSystem/src/FileSystem.c
+++ FileSystem/src/FileSystem.c
@@ -10,6 +10,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <assert.h>
 #include "FileSystem.h"
 
 int main(void) {
@@ -109,7 +111,7 @@ int main(void) {
 	fdmax = listener; // so far, it's this one
 
 	pthread_t hiloFileSystem;
-	//pthread_create(&hiloFileSystem, NULL, hiloFileSystem_Consola,NULL);
+	pthread_create(&hiloFileSystem, NULL, hiloFileSystem_Consola,NULL);
 
 	int socketActual;
 
@@ -146,77 +148,145 @@ int main(void) {
 	return EXIT_SUCCESS;
 }
 
+char** str_split(char* a_str, const char a_delim)
+{
+    char** result    = 0;
+    size_t count     = 0;
+    char* tmp        = a_str;
+    char* last_comma = 0;
+    char delim[2];
+    delim[0] = a_delim;
+    delim[1] = 0;
 
-//void hiloFileSystem_Consola(void * unused){
-//	printf("Consola Iniciada. Ingrese una opcion \n");
-//	char * linea;
-//	while(1) {
-//		linea = readline(">");
-//		if (!linea) {
-//			break;
-//		}else{
-//			add_history(linea);
-//			char* linea2 = strdup(linea);
-//			if (strcmp(linea, "format") == 0){
-//				printf("Formatear el Filesystem\n");
-//				free(linea);
-//			}else if (strcmp(strsep(&linea2, " "), "rm") == 0){
-//				printf("Eliminar un Archivo/Directorio/Bloque. Si un directorio a eliminar no se encuentra vacío, la operación debe fallar. Además, si el bloque a eliminar fuera la última copia del mismo, se deberá abortar la operación informando lo sucedido.\n");
-//				char *parametro = linea;
-//				strsep(&parametro, " ");
-//				printf(parametro);
-//				free(linea);
-//			}else if (strcmp(linea, "rename [path_original] [nombre_final]") == 0){
-//				printf("Renombra un Archivo o Directorio\n");
-//
-//				 char *linea2, *found;
-//				 char*parametros[2];
-//				 int i;
-//				 linea2 = strdup(linea);
-//				 found = strsep(&linea2," ");
-//				 found = strsep(&linea2," ");
-//
-//				 for(i=0;found != NULL;i++){
-//					 printf("%s\n",found);
-//					 parametros[i] = found;
-//					 found = strsep(&linea2," ");
-//				 }
-//
-//				free(linea);
-//			}else if (strcmp(linea, "mv [path_original] [path_final]") == 0){
-//				printf("Mueve un Archivo o Directorio\n");
-//				free(linea);
-//			}else if (strcmp(linea, "cat [path_archivo]") == 0){
-//				printf("Muestra el contenido del archivo como texto plano.\n");
-//				free(linea);
-//			}else if (strcmp(linea, "mkdir [path_dir]") == 0){
-//				printf("Crea un directorio. Si el directorio ya existe, el comando deberá informarlo.\n");
-//				free(linea);
-//			}else if (strcmp(linea, "cpfrom [path_archivo_origen] [directorio_yamafs]") == 0){
-//				printf("Copiar un archivo local al yamafs, siguiendo los lineamientos en la operaciòn Almacenar Archivo, de la Interfaz del FileSystem.\n");
-//				free(linea);
-//			}else if (strcmp(linea, "cpto [path_archivo_yamafs] [directorio_filesystem]") == 0){
-//				printf("Copiar un archivo local al yamafs\n");
-//				free(linea);
-//			}else if (strcmp(linea, "cpblock [path_archivo] [nro_bloque] [id_nodo]") == 0){
-//				printf("Crea una copia de un bloque de un archivo en el nodo dado.\n");
-//				free(linea);
-//			}else if (strcmp(linea, "md5 [path_archivo_yamafs]") == 0){
-//				printf("Solicitar el MD5 de un archivo en yamafs\n");
-//				free(linea);
-//			}else if (strcmp(linea, "ls [path_directorio]") == 0){
-//				printf("Lista los archivos de un directorio\n");
-//				free(linea);
-//			}else if (strcmp(linea, "info [path_archivo]") == 0){
-//				printf("Muestra toda la información del archivo, incluyendo tamaño, bloques, ubicación de los bloques, etc.\n");
-//				free(linea);
-//			}else {
-//				printf("Opcion no valida.\n");
-//				free(linea);
-//			}
-//		}
-//	}
-//}
+    /* Count how many elements will be extracted. */
+    while (*tmp)
+    {
+        if (a_delim == *tmp)
+        {
+            count++;
+            last_comma = tmp;
+        }
+        tmp++;
+    }
+
+    /* Add space for trailing token. */
+    count += last_comma < (a_str + strlen(a_str) - 1);
+
+    /* Add space for terminating null string so caller
+       knows where the list of returned strings ends. */
+    count++;
+
+    result = malloc(sizeof(char*) * count);
+
+    if (result)
+    {
+        size_t idx  = 0;
+        char* token = strtok(a_str, delim);
+
+        while (token)
+        {
+            assert(idx < count);
+            *(result + idx++) = strdup(token);
+            token = strtok(0, delim);
+        }
+        assert(idx == count - 1);
+        *(result + idx) = 0;
+    }
+
+    return result;
+}
+
+char** validaCantParametrosComando(char* comando, int cantParametros){
+	int i = 0;
+	char** parametros;
+	parametros = str_split(comando, ' ');
+	for (i = 1; *(parametros + i); i++)
+	{
+		printf("parametros= %s \n", *(parametros + i));
+	}
+	free(parametros);
+	if(i != cantParametros+1){
+		printf("rm necesita %i parametro/s. \n", cantParametros);
+		return 0;
+	}else{
+		printf("Cantidad de parametros correcta. \n");
+		return parametros;
+	}
+	return 0;
+}
+
+void hiloFileSystem_Consola(void * unused){
+	printf("Consola Iniciada. Ingrese una opcion \n");
+	char * linea;
+	char* primeraPalabra;
+	char* context;
+	int i = 0;
+	while(1) {
+		linea = readline(">");
+		if (!linea) {
+			break;
+		}else{
+			add_history(linea);
+			char** parametros;
+			int lineaLength = strlen(linea);
+			char *lineaCopia = (char*) calloc(lineaLength + 1, sizeof(char));
+			strncpy(lineaCopia, linea, lineaLength);
+			primeraPalabra = strtok_r (lineaCopia, " ", &context);
+
+			if (strcmp(linea, "format") == 0){
+				printf("Formatear el Filesystem\n");
+				free(linea);
+			}else if (strcmp(primeraPalabra, "rm") == 0){
+				printf("Eliminar un Archivo/Directorio/Bloque. Si un directorio a eliminar no se encuentra vacío, la operación debe fallar. Además, si el bloque a eliminar fuera la última copia del mismo, se deberá abortar la operación informando lo sucedido.\n");
+				parametros = validaCantParametrosComando(linea, 1);
+				free(linea);
+			}else if (strcmp(primeraPalabra, "rename") == 0){
+				printf("Renombra un Archivo o Directorio\n");
+				parametros = validaCantParametrosComando(linea, 2);
+				free(linea);
+			}else if (strcmp(primeraPalabra, "mv") == 0){
+				printf("Mueve un Archivo o Directorio\n");
+				parametros = validaCantParametrosComando(linea, 2);
+				free(linea);
+			}else if (strcmp(primeraPalabra, "cat") == 0){
+				printf("Muestra el contenido del archivo como texto plano.\n");
+				parametros = validaCantParametrosComando(linea, 1);
+				free(linea);
+			}else if (strcmp(primeraPalabra, "mkdir") == 0){
+				printf("Crea un directorio. Si el directorio ya existe, el comando deberá informarlo.\n");
+				parametros = validaCantParametrosComando(linea, 1);
+				free(linea);
+			}else if (strcmp(primeraPalabra, "cpfrom") == 0){
+				printf("Copiar un archivo local al yamafs, siguiendo los lineamientos en la operaciòn Almacenar Archivo, de la Interfaz del FileSystem.\n");
+				parametros = validaCantParametrosComando(linea, 2);
+				free(linea);
+			}else if (strcmp(primeraPalabra, "cpto") == 0){
+				printf("Copiar un archivo local al yamafs\n");
+				parametros = validaCantParametrosComando(linea, 2);
+				free(linea);
+			}else if (strcmp(primeraPalabra, "cpblock") == 0){
+				printf("Crea una copia de un bloque de un archivo en el nodo dado.\n");
+				parametros = validaCantParametrosComando(linea, 3);
+				free(linea);
+			}else if (strcmp(primeraPalabra, "md5") == 0){
+				printf("Solicitar el MD5 de un archivo en yamafs\n");
+				parametros = validaCantParametrosComando(linea, 1);
+				free(linea);
+			}else if (strcmp(primeraPalabra, "ls") == 0){
+				printf("Lista los archivos de un directorio\n");
+				parametros = validaCantParametrosComando(linea, 1);
+				free(linea);
+			}else if (strcmp(primeraPalabra, "info") == 0){
+				printf("Muestra toda la información del archivo, incluyendo tamaño, bloques, ubicación de los bloques, etc.\n");
+				parametros = validaCantParametrosComando(linea, 1);
+				free(linea);
+			}else {
+				printf("Opcion no valida.\n");
+				free(linea);
+			}
+		}
+	}
+}
 
 // SIN TERMINAR
 
