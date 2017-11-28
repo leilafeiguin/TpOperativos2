2dc6ad3f07c5071896c16d5ebd2a5930109abd55
lopezjazmin <jaz.lopez84@gmail.com>
2017-11-28 19:51:36 -0300

    Implementamos reduccion local

--- Worker/src/Worker.c
+++ Worker/src/Worker.c
@@ -44,7 +44,7 @@ void sortfile(char **array, int linecount){
 }
 
 
-char* apareo (char* paths []){
+void apareo (char* paths [], char* nombre_ordenado){
 	int cantPaths = sizeof(paths) / sizeof(paths[0]) + 1;
 	int i = 0;
 	unsigned long int linecountGlobal;
@@ -79,7 +79,7 @@ char* apareo (char* paths []){
 		fclose(fileIN);
 	}
 	sortfile(arrayGlobal, linecountGlobal);
-	FILE *archivoOrdenado = fopen("fileTestOrdenado", "wb");
+	FILE *archivoOrdenado = fopen(nombre_ordenado, "wb");
 	if(!archivoOrdenado){
 		printf("No se puede abrir el archivo.\n");
 		exit(-1);
@@ -91,7 +91,7 @@ char* apareo (char* paths []){
 	for(i=0; i<linecountGlobal; i++){
 		free(arrayGlobal[i]);
 	}
-	return "fileTestOrdenado";
+	return;
 }
 
 void* archivo;
@@ -126,73 +126,103 @@ int main(void) {
 	un_socket socketServer=socket_escucha("127.0.0.1", configuracion.PUERTO_WORKER);
 	listen(socketServer, 999);
 	while(1){
-		un_socket socketConexion=aceptar_conexion(socketServer);
-		if(socketConexion >0)
-		{
-			int pid=fork();
-				switch(pid)
+	un_socket socketConexion=aceptar_conexion(socketServer);
+	if(socketConexion >0)
+	{
+		int pid=fork();
+			switch(pid)
+			{
+				case -1: // Si pid es -1 quiere decir que es el proceso padre, ha habido un error
+					perror("No se ha podido crear el proceso hijo\n");
+					break;
+				case 0: // Cuando pid es cero quiere decir que es el proceso hijo
 				{
-					case -1: // Si pid es -1 quiere decir que es el proceso padre, ha habido un error
-						perror("No se ha podido crear el proceso hijo\n");
+					t_paquete* paquete_recibido = recibir(socketConexion);
+					esperar_handshake(socketConexion, paquete_recibido, cop_handshake_master);
+					switch(paquete_recibido->codigo_operacion){ //revisar validaciones de habilitados
+						case cop_handshake_master:
 						break;
-					case 0: // Cuando pid es cero quiere decir que es el proceso hijo
-					{
-						t_paquete* paquete_recibido = recibir(socketConexion);
-						esperar_handshake(socketConexion, paquete_recibido, cop_handshake_master);
-						switch(paquete_recibido->codigo_operacion){ //revisar validaciones de habilitados
-							case cop_handshake_master:
-							break;
-							case cop_handshake_worker:
-							break;
-							case cop_worker_transformacion:
-							{
-								int desplazamiento = 0;
-								int cantidadElementos = 0;
-								memcpy(&cantidadElementos, paquete_recibido->data, sizeof(int));
+						case cop_handshake_worker:
+						break;
+						case cop_worker_transformacion:
+						{
+							int desplazamiento = 0;
+							int cantidadElementos = 0;
+							memcpy(&cantidadElementos, paquete_recibido->data, sizeof(int));
+							desplazamiento += sizeof(int);
+							int i;
+							for(i=0;i<cantidadElementos;i++){
+								t_transf* paquete_transformacion = malloc(sizeof(t_transf));
+								//memcpy(origen, destino, cuantoQuieroCopiar)
+								memcpy(&paquete_transformacion->cant_script, paquete_recibido->data, sizeof(int));
 								desplazamiento += sizeof(int);
-								int i;
-								for(i=0;i<cantidadElementos;i++){
-									t_transf* paquete_transformacion = malloc(sizeof(t_transf));
-									//memcpy(origen, destino, cuantoQuieroCopiar)
-									memcpy(&paquete_transformacion->cant_script, paquete_recibido->data, sizeof(int));
-									desplazamiento += sizeof(int);
-									paquete_transformacion->script = malloc(paquete_transformacion->cant_script);
-									memcpy(paquete_transformacion->script, paquete_recibido->data + desplazamiento, paquete_transformacion->cant_script);
-									desplazamiento += paquete_transformacion->cant_script;
+								paquete_transformacion->script = malloc(paquete_transformacion->cant_script);
+								memcpy(paquete_transformacion->script, paquete_recibido->data + desplazamiento, paquete_transformacion->cant_script);
+								desplazamiento += paquete_transformacion->cant_script;
 
-									memcpy(&paquete_transformacion->bloq, paquete_recibido->data + desplazamiento, 1024*1024);
-									desplazamiento += sizeof(int);
-									paquete_transformacion->cant_archivo_temporal = 11;
-									paquete_transformacion->archivo_temporal = malloc(paquete_transformacion->cant_archivo_temporal);
-									memcpy(paquete_transformacion->archivo_temporal, paquete_recibido->data + desplazamiento, paquete_transformacion->cant_archivo_temporal);
-									desplazamiento += paquete_transformacion->cant_archivo_temporal;
-									memcpy(paquete_transformacion->cant_ocupada_bloque, paquete_recibido->data + desplazamiento, sizeof(int));
+								memcpy(&paquete_transformacion->bloq, paquete_recibido->data + desplazamiento, 1024*1024);
+								desplazamiento += sizeof(int);
+								paquete_transformacion->cant_archivo_temporal = 11;
+								paquete_transformacion->archivo_temporal = malloc(paquete_transformacion->cant_archivo_temporal);
+								memcpy(paquete_transformacion->archivo_temporal, paquete_recibido->data + desplazamiento, paquete_transformacion->cant_archivo_temporal);
+								desplazamiento += paquete_transformacion->cant_archivo_temporal;
+								memcpy(paquete_transformacion->cant_ocupada_bloque, paquete_recibido->data + desplazamiento, sizeof(int));
 
-									//paquete_transformacion->cant_script = *contenido (falta terminar)
-									FILE *archivoPaqueteTransformacion = fopen("./archivoPaqueteTransformacion", "wb");
-									fprintf(archivoPaqueteTransformacion,"%s", paquete_transformacion->script);
-									chmod("./archivoPaqueteTransformacion", 001); //permiso de ejecucion para ese path
-									transformacion(paquete_transformacion->script, obtenerBloque(paquete_transformacion->bloq, paquete_transformacion->cant_ocupada_bloque), paquete_transformacion->archivo_temporal);
-								}
+								//paquete_transformacion->cant_script = *contenido (falta terminar)
+								FILE *archivoPaqueteTransformacion = fopen("./archivoPaqueteTransformacion", "wb");
+								fprintf(archivoPaqueteTransformacion,"%s", paquete_transformacion->script);
+								chmod("./archivoPaqueteTransformacion", 001); //permiso de ejecucion para ese path
+								transformacion(paquete_transformacion->script, obtenerBloque(paquete_transformacion->bloq, paquete_transformacion->cant_ocupada_bloque), paquete_transformacion->archivo_temporal);
 							}
-							break;
-							case cop_worker_reduccionLocal:
-							break;
-							case cop_worker_reduccionGlobal:
-							break;
-							case -1:
-							{
-								printf("Se cayo Master, finaliza Worker.\n");
-								exit(-1);
-							}
-							break;
 						}
+						break;
+						case cop_worker_reduccionLocal:
+						{
+							int cantElementos;
+							int desplazamiento;
+							int i = 0;
+
+							memcpy(&cantElementos, paquete_recibido->data, sizeof(int));
+							desplazamiento += sizeof(int);
+
+							char* archivosAReducir[cantElementos];
+							for(; i < sizeof(archivosAReducir);i++) {
+								int tamanioElemento;
+								memcpy(&tamanioElemento, paquete_recibido->data + desplazamiento, sizeof(int));
+								desplazamiento += sizeof(int);
+
+								archivosAReducir[i] = malloc(tamanioElemento + 1);
+
+								memcpy(archivosAReducir[i], paquete_recibido->data + desplazamiento, tamanioElemento + 1); //tamanaio ele + 1 ??
+								desplazamiento += tamanioElemento;
+							}
+
+							int tamanio_archivo_reducido;
+							memcpy(&tamanio_archivo_reducido, paquete_recibido->data + desplazamiento, sizeof(int));
+							desplazamiento += sizeof(int);
+
+							char* archivo_reducido = malloc(tamanio_archivo_reducido);
+							memcpy(archivo_reducido, paquete_recibido->data + desplazamiento, tamanio_archivo_reducido + 1);
+
+							apareo(archivosAReducir, archivo_reducido);
+							//falta enviar respuesta a master consultar
+						}
+						break;
+						case cop_worker_reduccionGlobal:
+						break;
+						case -1:
+						{
+							printf("Se cayo Master, finaliza Worker.\n");
+							exit(-1);
+						}
+						break;
 					}
-				break;
-			}
+				}
+			break;
 		}
 	}
-	return EXIT_SUCCESS;
+}
+return EXIT_SUCCESS;
 }
 
 char* obtenerBloque(int numeroBloque, int tamanioBloque){
